a3db9118ddf04e0afe77c83565e1bb0158b7f9db
mstrauss <matthias.strauss@proton.me>
2024-09-29 22:56:20 +0200

    aded more tsts for ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 16:18:47 by mstrauss          #+#    #+#             */
-/*   Updated: 2024/09/24 18:14:16 by mstrauss         ###   ########.fr       */
+/*   Updated: 2024/09/29 22:54:35 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,19 @@ int main(void)
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-	std::cout << b << std::endl;
 
-	std::cout << Fixed::min(a, b) << std::endl;
+	std::cout << b << std::endl;
 	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "ADDITIONAL TESTS" << std::endl;
+	std::cout << "The bigger value of " << a << " and " << b << " is:" << Fixed::max(a, b) << std::endl;
+	std::cout << "The smaller value of " << a << " and " << b << " is:" << Fixed::min(a, b) << std::endl;
+	std::cout << std::endl;
+	std::cout << "The product of " << a << " and " << b << " is:" << a * b << std::endl;
+	std::cout << "The subtraction of " << a << " - " << b << " is:" << a - b << std::endl;
+	std::cout << "The division of " << a << " / " << b << " is:" << a / b << std::endl;
+	std::cout << "The sum of " << a << " + " << b << " is:" << a + b << std::endl;
 	return 0;
 }
